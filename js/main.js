ad9a3a5c8978e2b48b9458d30b302d8a78bb9645
Maxim Voropai <muckam49@mail.ru>
2017-10-01 23:56:16 +0800

    removed unnecessary code

--- js/main.js
+++ js/main.js
@@ -4,10 +4,3 @@ import artist from './screens/artist';
 import resultWin from './screens/result-win';
 import resultTimeIsOver from './screens/result-time-is-over';
 import resultAttemptsEnded from './screens/result-attempts-ended';
-
-
-/*const KEY_CODES = {
-  ALT: 18,
-  ARROW_RIGHT: 39,
-  ARROW_LEFT: 37
-};*/
