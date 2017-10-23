ecf8fff3fb68fc6855f517afddb244a062908ed7
Maxim Voropai <muckam49@mail.ru>
2017-10-24 02:08:01 +0800

    commented code deleted

--- js/screens/game/game.js
+++ js/screens/game/game.js
@@ -1,22 +1,3 @@
-/* import GameView from '../../views/game-view';
-import {stateGame} from '../../data/data';
-import displayAmountMistakes from '../../lib/displayAmountMistakes';
-import displayTimer from '../../lib/displayTimer';
-import showLevel from './level';
-import setStateGame from '../../lib/setStateGame';
-
-const game = new GameView();
-game.init = () => {
-  setStateGame();
-  stateGame.answers.splice(0, 10);
-  displayTimer();
-  displayAmountMistakes(stateGame.lives);
-
-  showLevel().init();
-};
-
-export default () => game;*/
-
 import GameView from '../../views/game-view';
 import setStateGame from '../../lib/setStateGame';
 import {stateGame} from '../../data/data';
